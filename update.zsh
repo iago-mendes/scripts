11293b8eea0b2a629fa1202edd2dc904732dde9e
Iago Braz Mendes <iagobrazmendes@gmail.com>
2020-08-22 12:06:18 -0300

    rclone added to deal with drive

--- update.zsh
+++ update.zsh
@@ -1,13 +1,13 @@
-ONLINE_DRIVE="/run/user/1000/gvfs/google-drive:host=oberlin.edu,user=ibrazmen"
+ONLINE_DRIVE="Drive:"
 OFFLINE_DRIVE="/home/iago/Desktop/Offline Drive"
 
 function update()
 {
     if [ "$1" = "drive" ]; then
         if [ "$2" = "from" ]; then
-            cp -r -u $ONLINE_DRIVE $OFFLINE_DRIVE
+            rclone copy $ONLINE_DRIVE $OFFLINE_DRIVE
         else
-            cp -r -u $OFFLINE_DRIVE $ONLINE_DRIVE
+            rclone copy $OFFLINE_DRIVE $ONLINE_DRIVE
         fi
     elif [ "$1" = "system" ]; then
         sudo apt update
