286d92703989b2c880d4618ea13ac6b8ec1d117f
Iago Braz Mendes <iagobrazmendes@gmail.com>
2020-08-22 10:43:55 -0300

    quick commands

--- commands.zsh
+++ commands.zsh
@@ -1,6 +1,14 @@
-function commands()
+# QUICK COMMANDS
+
+function commands() # open this file
 {
 	code ~/Desktop/Coding/scripts/commands.zsh
+}
+
+function git-ac() # git add and commit
+{
+	git add .
+	git commit -m "$1"
 }
 
 source '/home/iago/Desktop/Coding/scripts/start.zsh'
