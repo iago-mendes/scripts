bd14a9ba399440b0146b6cacd2cd578362222d5e
Iago <iagobrazmendes@gmail.com>
2020-12-16 18:14:52 -0300

    organize code

--- functions/update.zsh
+++ functions/update.zsh
@@ -3,17 +3,17 @@ OFFLINE_DRIVE="/home/iago/Drive"
 
 function update()
 {
-    if [ "$1" = "drive" ]; then
-        if [ "$2" = "from" ]; then
-            rclone copy $ONLINE_DRIVE $OFFLINE_DRIVE --max-size 500M --verbose --stats 500ms
-        else
-            rclone sync $OFFLINE_DRIVE $ONLINE_DRIVE --verbose --stats 500ms
-        fi
-    elif [ "$1" = "system" ]; then
-        sudo apt update
-        sudo apt upgrade -y
-        sudo apt autoremove -y
-    else
-        echo "$1 is not an option of the 'update' command."
-    fi
+	if [ "$1" = "drive" ]; then
+		if [ "$2" = "from" ]; then
+			rclone copy $ONLINE_DRIVE $OFFLINE_DRIVE --max-size 500M --verbose --stats 500ms
+		else
+			rclone sync $OFFLINE_DRIVE $ONLINE_DRIVE --verbose --stats 500ms
+		fi
+	elif [ "$1" = "system" ]; then
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt autoremove -y
+	else
+		echo "$1 is not an option of the 'update' command."
+	fi
 }
