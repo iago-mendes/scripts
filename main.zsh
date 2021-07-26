b754d91f3f6b389efd102e15e24b4c0b51dcaadf
Iago <iagobrazmendes@gmail.com>
2021-07-26 18:08:23 -0300

    chore: organize code

---
+++ main.zsh
@@ -0,0 +1,13 @@
+source "$HOME/Coding/scripts/.env.zsh"
+source "$HOME/Coding/scripts/paths.zsh"
+
+function git-ac() # git add and commit
+{
+	git add .
+	git commit -m "$1"
+}
+
+source "$HOME/Coding/scripts/functions/start.zsh"
+source "$HOME/Coding/scripts/functions/update.zsh"
+source "$HOME/Coding/scripts/functions/vpn.zsh"
+source "$HOME/Coding/scripts/functions/connect.zsh"
