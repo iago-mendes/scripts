b1f66f22ca88ddf79fdfb1f1c59825e84abafa60
Iago <iagobrazmendes@gmail.com>
2021-08-04 12:10:45 -0300

    chore: work on post install

--- post-install.zsh
+++ post-install.zsh
@@ -12,7 +12,7 @@ curl -fsSL -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/myTheme.zsh-theme https:
 curl -fsSL -o ~/.zshrc https://raw.githubusercontent.com/iago-mendes/scripts/master/terminal/.zshrc
 source ~/.zshrc
 
-# coding tools
+# coding tools => nodejs, yarn, expo, vercel, docker
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install --global yarn
@@ -25,8 +25,38 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# coding apps
-sudo apt install -y vim
+# coding apps => vim, texmaker, vscode, insonmia
+sudo apt install -y vim texmaker
 curl -fsSL -o ~/Downloads/vscode.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
 sudo dpkg -i ~/Downloads/vscode.deb
 rm ~/Downloads/vscode.deb
+curl -L https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website > ~/Downloads/insomnia.deb
+sudo dpkg -i ~/Downloads/insomnia.deb
+rm ~/Downloads/insomnia.deb
+
+# useful apps => notion, opera
+curl -L https://raw.githubusercontent.com/puneetsl/lotion/master/setup.sh > ~/Downloads/notion.sh
+chmod +x ~/Downloads/notion.sh
+sudo ~/Downloads/notion.sh native
+rm ~/Downloads/notion.sh
+curl -L https://download.opera.com/download/get/?partner=www&opsys=Linux > ~/Downloads/opera.deb
+sudo dpkg -i ~/Downloads/opera.deb
+rm ~/Downloads/opera.deb
+curl -L https://raw.githubusercontent.com/iago-mendes/setup-widevine-linux/main/download/libffmpeg.so > ~/Downloads/libffmpeg.so
+sudo cp libffmpeg.so /usr/lib/x86_64-linux-gnu/opera
+rm ~/Downloads/libffmpeg.so
+
+# audiovisual apps => audacity, gimp, inkscape, obs, kdenlive
+sudo add-apt-repository ppa:ubuntuhandbook1/audacity
+sudo add-apt-repository ppa:inkscape.dev/stable-1.1
+sudo apt install -y ffmpeg
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+sudo apt-get update
+sudo apt install -y audacity gimp inkscape obs-studio kdenlive
+
+# stem apps => stellarium, geogebra
+if [ ! -d "~/Apps" ]; then
+	mkdir ~/Apps
+fi
+curl -L https://github.com/Stellarium/stellarium/releases/download/v0.21.1/stellarium-0.21.1-x86_64.AppImage > ~/Apps/Stellarium.AppImage
