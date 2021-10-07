c885006624d367e50b00bd44ccaeb5244bb0d564
Iago <iago@pop-os.localdomain>
2021-10-07 09:47:15 -0400

    feat: update connect function

--- functions/connect.zsh
+++ functions/connect.zsh
@@ -5,6 +5,8 @@ function connect()
 			ssh -i $STARVIEW_KEY_PATH iago@$STARVIEW_IP
 		elif [ "$2" = "digital-ocean" ]; then
 			ssh iago@$DIGITAL_OCEAN_IP
+		elif [ "$2" = "cs-oberlin" ]; then
+			sshpass -p $OBERLIN_CS_PASS ssh $OBERLIN_CS_USER@$OBERLIN_CS_ADDRESS
 		else
 			echo "$2 is not an option of available ssh connections"
 		fi
