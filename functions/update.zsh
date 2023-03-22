9e56f00a5a830c868f8bee7d49975d5fb3c7df78
Iago Mendes <iagobrazmendes@gmail.com>
2023-03-21 21:33:24 -0400

    Add flags to skip links when copying to/from drive.

--- functions/update.zsh
+++ functions/update.zsh
@@ -2,9 +2,9 @@ function update()
 {
 	if [ "$1" = "drive" ]; then
 		if [ "$2" = "from" ]; then
-			rclone copy $ONLINE_DRIVE $OFFLINE_DRIVE --max-size 100M --verbose --stats 500ms
+			rclone copy $ONLINE_DRIVE $OFFLINE_DRIVE --max-size 100M --verbose --stats 500ms --drive-skip-shortcuts
 		else
-			rclone copy $OFFLINE_DRIVE $ONLINE_DRIVE --verbose --stats 500ms
+			rclone copy $OFFLINE_DRIVE $ONLINE_DRIVE --verbose --stats 500ms --sftp-skip-links
 		fi
 	elif [ "$1" = "system" ]; then
 		sudo apt update
