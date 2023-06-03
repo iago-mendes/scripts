4781cdf2bb063455c5f5fcffa30b19293c808a5c
Iago Mendes <ibrazmen@oberlin.edu>
2023-06-03 16:47:01 -0700

    Add SpEC binaries to PATH.

--- paths.zsh
+++ paths.zsh
@@ -5,6 +5,11 @@ if [ -d ~/Apps/bin ]; then
   PATH=$PATH:~/Apps/bin
 fi
 
+# SpEC
+if [ -d ~/Coding/spec/Support/bin ]; then
+  PATH=$PATH:~/Coding/spec/Support/bin
+fi
+
 # Apple's Transporter
 if [ -d /usr/local/itms/bin/ ]; then
   PATH=$PATH:/usr/local/itms/bin/
